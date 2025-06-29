7b886d2645d6942ea316bbbab6b6e91c177dd0d4
frc <frc@ElbowsUps-MacBook-Air.local>
2025-06-29 13:55:42 -0600

    new wrist pivot

--- src/main/java/frc/robot/subsystems/StingerSubsystem.java
+++ src/main/java/frc/robot/subsystems/StingerSubsystem.java
@@ -19,6 +19,13 @@ public class StingerSubsystem extends SubsystemBase {
     private AbsoluteEncoder m_absoluteEncoder;
     private PIDController m_pivotPID;
     public double pivotOutput = 0;
+    private SparkMax m_wristPivotMotor; 
+    private SparkMaxConfig wristPivotConfig; 
+    private AbsoluteEncoder m_wristAbsoluteEncoder;
+    private PIDController m_wristPivotPID;
+    public double wristPivotOutput = 0;
+
+
 
     // Constructors
     public StingerSubsystem() {
@@ -30,10 +37,18 @@ public class StingerSubsystem extends SubsystemBase {
         
         m_absoluteEncoder =  m_pivotMotor.getAbsoluteEncoder();
         m_pivotPID = new PIDController(Constants.PivotConstants.pivotKP,Constants.PivotConstants.pivotKI, Constants.PivotConstants.pivotKD);
+    
+        m_wristPivotMotor = new SparkMax(Constants.PivotConstants.kPivotMotorId/*CHANGE ID */, MotorType.kBrushless);
+        wristPivotConfig = new SparkMaxConfig();
+        wristPivotConfig.idleMode(IdleMode.kBrake);
+        wristPivotConfig.inverted(true);
+        m_wristPivotMotor.configure(wristPivotConfig, null, null);
+        
+        m_wristAbsoluteEncoder =  m_wristPivotMotor.getAbsoluteEncoder();
+        m_wristPivotPID = new PIDController(Constants.PivotConstants.wristPivotKP,Constants.PivotConstants.wristPivotKI, Constants.PivotConstants.wristPivotKD);
     }
 
 
-//hi :)
     public void setPivotPower(double pivotPower){
         m_pivotMotor.set(pivotPower);
     }
@@ -41,8 +56,13 @@ public class StingerSubsystem extends SubsystemBase {
     public void pivotPIDControl(double targetAngle) {
         pivotOutput = m_pivotPID.calculate(m_absoluteEncoder.getPosition(), targetAngle);
         m_pivotMotor.set(pivotOutput);  
-
         //System.out.println("pivot pid ran, target angle was" + targetAngle);
+    }
+    public void wristPivotPIDControl(double targetWristAngle) {
+        //wristPivotOutput = m_wristPivotPID.calculate(m_wristAbsoluteEncoder.getPosition(), targetWristAngle);
+        //m_wristPivotMotor.set(wristPivotOutput);  
+
+        System.out.println("pivot pid ran, target angle was" + targetWristAngle);
     }
 
      public double encoderGetValue(){
